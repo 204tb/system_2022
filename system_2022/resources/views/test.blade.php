50c9d5faf078292958b226b7117990ecff4d2392
204tb <200204tb@yse-c.net>
2022-02-28 14:09:40 +0900

    入力フォームを追加

--- system_2022/resources/views/test.blade.php
+++ system_2022/resources/views/test.blade.php
@@ -8,11 +8,14 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
-    <div class="container pt-4">
-        <h2 class="h2">情報入力</h2>
-        <form action="">
-            
-            <input type="text" name="name" id="name" class="form-control-sm">
+
+    <div class="container pt-4 bg-light">
+    <h2 class="h2">情報入力</h2>
+        <form action="" class ="ml-5 form-inline">    
+            <p class="mr-4 mt-2"><label for="name">商品名</label></p>
+            <input type="text" name="name" id="name" class="form-control-sm mb-2 mr-5">
+            <p class="mr-4 mt-2"><label for="name">価格</label></p>
+            <input type="text" name="name" id="name" class="form-control-sm mb-2">
         </form>
     </div>
 </body>
